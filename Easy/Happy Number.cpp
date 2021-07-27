9d1cb75980229f4a1a3461e5babba94f2fe30dee
Alok Khansali <75980074+TheCodeAlpha26@users.noreply.github.com>
2021-07-27 22:44:01 +0530

    Create Happy Number.cpp

---
+++ Easy/Happy Number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        int r,s=100;
+        while(s>9)
+       {
+          s=0;
+          while(n>0)
+          {
+              r=n%10;
+              s=s+r*r;
+              n=n/10;
+            }
+            n=s;
+            if(s==7)
+                return(true);
+        }
+       return(s==1);
+    }
+};
