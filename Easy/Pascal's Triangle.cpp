f8ed768d5538057ea6781dcdd4673c74a4583900
Alok Khansali <75980074+TheCodeAlpha26@users.noreply.github.com>
2021-09-10 20:13:24 +0530

    Create Pascal's Triangle.cpp

---
+++ Easy/Pascal's Triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int r) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        vector<vector<int>> v(r);
+        v[0].push_back(1);
+        for(int i=2;i<=r;i++)
+        {
+            v[i-1].resize(i);
+            v[i-1][0] = 1, v[i-1][i-1] = 1;
+            for(int j=1;j<i-1;j++)
+                v[i-1][j] = v[i-2][j] + v[i-2][j-1];
+        }
+        return(v);
+    }
+};
