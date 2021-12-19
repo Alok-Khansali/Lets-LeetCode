3b538675c361c2c5a8b371c82215920366a4e088
Alok Khansali <75980074+TheCodeAlpha26@users.noreply.github.com>
2021-12-19 23:20:41 +0530

    Create Power Of Three.cpp

---
+++ Easy/Power Of Three.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) 
+    {
+        ios_base::sync_with_stdio(false);
+        if(n <= 0)
+            return false;
+        double x = log10(n)/log10(3);                     //Get log at any base using log10(number)/log(base)
+        return (x == floor(x));
+    }
+};
