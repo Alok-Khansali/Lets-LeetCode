e292da417801f69e3a35491a1838bb272835f4eb
Alok Khansali <alokkhansali2002@gmail.com>
2021-12-26 18:10:57 +0530

    Med Correction

---
+++ Medium/Basic Calculator II.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int calculate(string& s) 
+    {
+        int Current = 0, res = 0, temp = 0;
+        char PrevChar = '+';
+        s += "xx";                                     
+        for(auto c : s)
+        {
+            if(c == ' ') 
+            continue;
+            else if(isdigit(c)) 
+                Current = Current * 10 + (c-'0');
+            else 
+            {
+                if(PrevChar == '*')                      // Multiply
+                    temp *= Current;
+                else if(PrevChar == '/')                 // Divide
+                    temp /= Current;
+                else  
+                {                                        // Add or Subtract
+                    res += temp;
+                    temp = ((PrevChar == '+') ? Current : -Current);
+                }
+                PrevChar = c;
+                Current = 0;
+            }
+        }
+        return res;
+    }
+};
--- Medium/K Closest Points to Origin.cpp
+++ Medium/K Closest Points to Origin.cpp
@@ -1,26 +1,19 @@
-class Solution {    //Runtime: 120 ms, faster than 99.28% of C++ online submissions for K Closest Points to Origin.
+class Solution {    //Runtime: 112 ms, faster than 99.66% of C++ online submissions for K Closest Points to Origin.
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) 
     {
         ios_base::sync_with_stdio(false);
-	      cin.tie(NULL);
-      
+	    cin.tie(NULL);
         vector<pair<double,pair<int,int>>> ar;
         vector<vector<int>> answer;
-      
         for(int i=0;i<points.size();i++)
         {
-            double x = points[i][0], y = points[i][1], c = sqrt(x*x + y*y);
+            double x = points[i][0], y = points[i][1], c = x*x + y*y;   //Getting the distance from the origin
             ar.push_back({c,{x,y}});
         }
-      
-        sort(ar.begin(),ar.end());
-      
+        sort(ar.begin(),ar.end());                                      //Sort according to the euclidian distance
         for(int i=0;i<k;i++)
-        {
             answer.push_back({ar[i].second.first,ar[i].second.second});
-        }
-      
         return answer;
     }
 };
